fce12d6716f2e16847e33994647c6201520e6d69
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-11 15:36:46 +0300

    add navbar

--- project/client/src/app/app.module.ts
+++ project/client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
