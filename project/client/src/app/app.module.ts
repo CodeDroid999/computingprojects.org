efcb8a4a85917e9044da5924641a3948415bf8cd
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-11 19:23:59 +0300

    deploy

--- project/client/src/app/app.module.ts
+++ project/client/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavbarComponent,
-    HomeComponent
+    AboutComponent
   ],
   imports: [
     BrowserModule,
