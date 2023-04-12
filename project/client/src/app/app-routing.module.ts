448e9e24dd58f2056ee7e303b37a8ef95744648e
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-12 20:53:00 +0300

    correct navigation

--- project/client/src/app/app-routing.module.ts
+++ project/client/src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { ServicesComponent } from './components/services/services.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { BlogComponent } from './components/blog/blog.component';
 
-const routes: Routes = [
-  {path: '', component:HomeComponent},
+const routes: Routes = [  
   {path:'/about-us', component:AboutComponent},
   {path:'/services', component:ServicesComponent},
   {path:'/contact', component:ContactComponent},
-  {path:'/blog', component:BlogComponent}
+  {path:'/blog', component:BlogComponent},
+  {path: '', component:HomeComponent},
 ];
 
 @NgModule({
