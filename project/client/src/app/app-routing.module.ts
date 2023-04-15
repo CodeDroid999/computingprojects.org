f7e40561ceb7096c419a85932d922c3b3208b6a2
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-15 11:02:54 +0300

    make navbar sticky

--- project/client/src/app/app-routing.module.ts
+++ project/client/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { ContactComponent } from './components/contact/contact.component';
 import { BlogComponent } from './components/blog/blog.component';
 
 const routes: Routes = [  
-  {path:'/about-us', component:AboutComponent},
-  {path:'/services', component:ServicesComponent},
-  {path:'/contact', component:ContactComponent},
-  {path:'/blog', component:BlogComponent},
+  {path:'about-us', component:AboutComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'blog', component:BlogComponent},
   {path: '', component:HomeComponent}
 ];
 
