37d8092c703698cd97c0632033694466e116af7d
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-15 13:14:42 +0300

    add ts

--- project/client/src/main.ts
+++ project/client/src/main.ts
@@ -1,10 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-const navbar = document.querySelector('nav') as HTMLElement;
-navbar.style.backgroundColor = 'black';
+
+
