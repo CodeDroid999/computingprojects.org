85b736393b4920c26c0e8989cee3b85371811a97
CodeDroid99 <johnmwega2002@gmail.com>
2023-04-29 00:23:06 +0300

    rerun ng serve

--- project/client/src/app/components/blue-nav/blue-nav.component.ts
+++ project/client/src/app/components/blue-nav/blue-nav.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class BlueNavComponent {
 
+  header_variable = false;
+  @HostListener('document:scroll')
+  scrollFunction() {
+    if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 0) {
+      this.header_variable = true;
+    } else {
+      this.header_variable = false;
+    }
+  }
 }
+function HostListener(arg0: string): (target: BlueNavComponent, propertyKey: "scrollFunction", descriptor: TypedPropertyDescriptor<() => void>) => void | TypedPropertyDescriptor<() => void> {
+  throw new Error('Function not implemented.');
+}
+
